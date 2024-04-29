c4a4eb911873343cf033c16ab6277675d6b54caf
Niloy Das <niloydas5215@gmail.com>
2024-04-29 23:43:33 +0600

    add tooltip in the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,14 +3,14 @@ import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProviders";
 // import navBackground from "../../assets/background.svg";
 // import "animate.css";
-
+import { Tooltip } from 'react-tooltip';
 
 const Navbar = () => {
     const { user, logOut, loading, isDark, setIsDark } = useContext(AuthContext);
 
     useEffect(() => {
         localStorage.setItem('theme', isDark);
-      }, [isDark]);
+    }, [isDark]);
 
     if (loading) {
         return (
@@ -40,6 +40,7 @@ const Navbar = () => {
         <li><NavLink to={`/my-list/${user?.email}`}>My List</NavLink></li>
     </>
 
+
     return (
         // style={{ backgroundImage: `url(${navBackground})` }}
         <div className={`navbar flex-no-wrap ${isDark === 'dark' ? "bg-[#150d32]" : "bg-white"} top-0 z-10 fixed `}>
@@ -53,7 +54,7 @@ const Navbar = () => {
                             user ?
                                 <>
                                     <li className="space-y-2 md:hidden">
-                                        <div className="avatar tooltip tooltip-bottom" data-tip={user.displayName}>
+                                        <div className="avatar" data-tooltip-id="my-tooltip">
                                             <div className="w-8 md:w-11 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
                                                 <img src={user.photoURL ? user.photoURL : "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"} />
                                             </div>
@@ -87,16 +88,16 @@ const Navbar = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
             </label>
             <div className="space-x-3 navbar-end hidden md:flex">
-            <label className="flex cursor-pointer gap-2 navbar-end">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
-                <input type="checkbox" value="synthwave" className="toggle theme-controller" onChange={() => { (isDark === 'dark') ? setIsDark('light') : setIsDark('dark') }} />
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
-            </label>
+                <label className="flex cursor-pointer gap-2 navbar-end">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
+                    <input type="checkbox" value="synthwave" className="toggle theme-controller" onChange={() => { (isDark === 'dark') ? setIsDark('light') : setIsDark('dark') }} />
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+                </label>
                 {
                     user ?
                         <>
                             <div className="flex items-center space-x-2">
-                                <div className="avatar tooltip tooltip-bottom" data-tip={user.displayName}>
+                                <div className="avatar" data-tooltip-id="my-tooltip">
                                     <div className="w-8 md:w-11 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
                                         <img src={user.photoURL ? user.photoURL : "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"} />
                                     </div>
@@ -111,6 +112,10 @@ const Navbar = () => {
                         </>
                 }
             </div>
+            <Tooltip
+                id="my-tooltip"
+                content={user.displayName}
+            />
         </div>
     );
 };

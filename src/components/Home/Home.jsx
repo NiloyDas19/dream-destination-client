8b8167ce34a3f8c74a81ab2faaaeb48c87316a45
Niloy Das <niloydas5215@gmail.com>
2024-04-30 15:03:11 +0600

    react-awsome-reveal

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,6 +5,8 @@ import { useContext, useEffect, useState } from "react";
 import CountryCard from "../CountryCard/CountryCard";
 import { AuthContext } from "../../providers/AuthProviders";
 import DocumentTitle from "../../documentTitle/DocumentTitle";
+import { Fade } from "react-awesome-reveal";
+
 
 const Home = () => {
     DocumentTitle('Home');
@@ -14,7 +16,7 @@ const Home = () => {
     const { isDark } = useContext(AuthContext);
 
     useEffect(() => {
-        fetch('https://dream-destination-server-side.vercel.app/countries',{credentials: "include"})
+        fetch('https://dream-destination-server-side.vercel.app/countries', { credentials: "include" })
             .then(res => res.json())
             .then(data => {
                 console.log(data);
@@ -33,7 +35,9 @@ const Home = () => {
             {/* Tourists Spot */}
             <div className="mt-10 space-y-10 w-[95%] mx-auto">
                 <div className="text-center space-y-2">
-                    <h1 className="text-center font-bold text-3xl">Our Tourists Spots</h1>
+                    <Fade>
+                        <h1 className="text-center font-bold text-3xl">Our Tourists Spots</h1>
+                    </Fade>
                     <p>
                         Discover the enchanting beauty of Southeast Asia with our curated selection of top tourist spots. From the pristine beaches of Cox's Bazar in Bangladesh to the ancient temples of Borobudur in Indonesia, immerse yourself in the diverse landscapes, rich cultures, and vibrant experiences that await in this captivating region. Explore iconic landmarks, natural wonders, and hidden gems across Bangladesh, Thailand, Indonesia, Malaysia, Vietnam, and Cambodia.</p>
                 </div>
@@ -47,7 +51,9 @@ const Home = () => {
             {/* Countries Section */}
             <div className="mt-10 space-y-10 w-[95%] mx-auto">
                 <div className="text-center space-y-2">
-                    <h1 className="text-center font-bold text-3xl">Countries</h1>
+                    <Fade>
+                        <h1 className="text-center font-bold text-3xl">Countries</h1>
+                    </Fade>
                     <p>
                         Embark on a journey through Southeast Asia's diverse tapestry of cultures and landscapes. From the lush hills of Bangladesh to the pristine beaches of Thailand, the ancient temples of Indonesia to the modern metropolis of Malaysia, Vietnam's emerald waters to the historic wonders of Cambodia, our countries section invites you to explore the vibrant tapestry of this enchanting region. Discover the unique charm, rich heritage, and breathtaking beauty of each country, and let your wanderlust guide you to unforgettable adventures.
                     </p>
@@ -62,7 +68,9 @@ const Home = () => {
             {/* Most Visited Tourists Spot */}
             <div className="mt-10 space-y-10 w-[95%] mx-auto">
                 <div className="text-center space-y-2">
-                    <h1 className="text-center font-bold text-3xl">Most Visited Tourists Spot</h1>
+                    <Fade>
+                        <h1 className="text-center font-bold text-3xl">Most Visited Tourists Spot</h1>
+                    </Fade>
                     <p>
                         Uncover the allure of Southeast Asia's most visited tourist spots, drawing millions of visitors annually to its breathtaking locales. From the majestic temples of Angkor Wat in Cambodia to the pristine beaches of Phuket in Thailand, each destination promises a captivating journey filled with cultural immersion, natural wonders, and unforgettable experiences. Explore these iconic sites and indulge in the region's rich heritage, diverse landscapes, and warm hospitality, creating memories that last a lifetime.
                     </p>
@@ -77,7 +85,9 @@ const Home = () => {
             {/* Stats */}
             <div className="mt-10 space-y-10 w-[95%] mx-auto">
                 <div className="text-center space-y-2">
-                    <h1 className="text-center font-bold text-3xl">Stats</h1>
+                    <Fade>
+                        <h1 className="text-center font-bold text-3xl">Stats</h1>
+                    </Fade>
                     <p>
                         Delve into Southeast Asia's tourism landscape with our Stats section, offering insights into the region's total countries, myriad tourist spots, and the pulse of its most visited destinations. Navigate through data-driven metrics, uncovering the region's vast cultural diversity, natural wonders, and popular attractions, guiding travelers towards memorable adventures across this enchanting destination.
                     </p>
